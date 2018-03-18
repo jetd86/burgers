2a15bcaaf162b97bbacad7effb8b8fd6725c17ab
jetdan <jetdan86@ya.ru>
2018-03-18 19:59:11 +0300

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'burgers', '4rTnCQSzwk0', 'burgers'); //устанавливаем соединение
+$mysqli = new mysqli('localhost', 'burgers', '', 'burgers'); //устанавливаем соединение
 mysqli_query("SET NAMES 'UTF-8'");
 if (mysqli_connect_errno()) { //проверк на ошибку
     printf('Ошибка соединения: ', mysqli_connect_errno());
